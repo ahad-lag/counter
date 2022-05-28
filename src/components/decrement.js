6739318bbefa21185d99a2fa5927f28ad03f24a8
Ahad Lag <mr.ahad.lag@gmail.com>
2022-05-28 19:32:02 +0430

    complete decrement

--- src/components/decrement.js
+++ src/components/decrement.js
@@ -1,8 +1,15 @@
+import { useDispatch } from 'react-redux';
+import { decrement } from './../store/slices/counterSlice';
 
 export default function Decrement(){
+
+    const dispatch = useDispatch();
+    const decrementHandler = () => dispatch(decrement())
+
     return(
         <button
             name="decrement"
+            onClick={decrementHandler}
             className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 mt-3"
             >
             کم کردن
